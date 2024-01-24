bd32925a90d6817d2e3db1a2f3277cdf358bb383
matbed98 <bednarekmateusz98@gmail.com>
2024-01-24 01:13:21 +0100

    Set Up and Import Database Configuration from .env File + There was no database configuration in the app.js file, which made it impossible to do anything with database. Configuration was set in .env file to allow app load the appropriate settings based on the environment variables.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {databaseConfig, validationSchema} from './config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env',
+            isGlobal: true,
+            validationSchema: validationSchema,
+        }),
+        TypeOrmModule.forRoot(databaseConfig()),
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
---
+++ src/config/index.ts
@@ -0,0 +1,2 @@
+export {default as databaseConfig} from './database.config';
+export * from './validation.config';
